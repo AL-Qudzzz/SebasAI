a33a9b83eab89a1c2af23a7e5acf5c6d3274a4eb
AL Qudzzz <faiqul84@gmail.com>
2025-06-13 08:19:03 +0000

    optimize Tampilan UI nya agar responsif

--- src/components/common/PageTitle.tsx
+++ src/components/common/PageTitle.tsx
@@ -9,8 +9,8 @@ interface PageTitleProps {
 export default function PageTitle({ title, description, className }: PageTitleProps) {
   return (
     <div className={`mb-8 ${className}`}>
-      <h1 className="text-4xl font-headline font-semibold text-primary mb-2">{title}</h1>
-      {description && <p className="text-lg text-muted-foreground">{description}</p>}
+      <h1 className="text-3xl sm:text-4xl font-headline font-semibold text-primary mb-2">{title}</h1>
+      {description && <p className="text-base sm:text-lg text-muted-foreground">{description}</p>}
     </div>
   );
 }
